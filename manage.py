8fff5b371f63892e8cbe8c3060d0eebe575a0ab6
Meitian Huang <_@freeaddr.info>
2015-09-07 10:33:37 +1000

    Revert "Use webassets"
    This reverts commit 8a78a0ae880b249cd8d3a106ee8aab080425d151.

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ from app import create_app, db
 from app.models import Variation, Project, Item
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
-from flask.ext.assets import ManageAssets
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -16,7 +15,6 @@ def make_shell_context():
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
-manager.add_command("assets", ManageAssets())
 
 if __name__ == '__main__':
     manager.run()
