dfb4a81c4a683ef46cc8040cdda7229568151aa3
Meitian Huang <_@freeaddr.info>
2015-09-08 16:36:45 +1000

    Add the missing file...

---
+++ app/api_1_0/progress_items.py
@@ -0,0 +1,51 @@
+from flask import jsonify, request
+
+from . import api
+from .. import db
+from .authentication import auth
+from ..models import ProgressItem
+
+
+@api.route('/progress_items/')
+@auth.login_required
+def get_progress_items():
+    progress_items = ProgressItem.query.all()
+    return jsonify({'progress_items': [progress_item.to_json() for progress_item in progress_items]})
+
+
+@api.route('/progress_items/<int:item_id>')
+@auth.login_required
+def get_progress_item(item_id):
+    progress_item = ProgressItem.query.get_or_404(item_id)
+    return jsonify(progress_item.to_json())
+
+
+@api.route('/progress_items/', methods=['POST'])
+@auth.login_required
+def new_progress_item():
+    progress_item = ProgressItem.from_json(request.json)
+    db.session.add(progress_item)
+    db.session.commit()
+    return jsonify(progress_item.to_json()), 201
+
+
+@api.route('/progress_items/<int:item_id>', methods=['PUT'])
+@auth.login_required
+def edit_progress_item(item_id):
+    progress_item = ProgressItem.query.get_or_404(item_id)
+    """:type : ProgressItem"""
+    progress_item.completed_value = request.json.get('completed_value', progress_item.completed_value)
+    progress_item.contract_value = request.json.get('contract_value', progress_item.contract_value)
+    progress_item.name = request.json.get('name', progress_item.name)
+    db.session.add(progress_item)
+    db.session.commit()
+    return jsonify(progress_item.to_json())
+
+
+@api.route('/progress_items/<int:item_id>', methods=['DELETE'])
+@auth.login_required
+def delete_progress_item(item_id):
+    progress_item = ProgressItem.query.get_or_404(item_id)
+    db.session.delete(progress_item)
+    db.session.commit()
+    return jsonify(progress_item.to_json())
