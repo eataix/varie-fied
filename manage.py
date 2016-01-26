84e6b95d688848e2b2bc3c502142dacadce0fcb0
Meitian Huang <_@freeaddr.info>
2016-01-26 15:32:51 +1100

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager, Shell
 from app import create_app, db
-from app.models import Variation, Project, Item, ProgressItem, Client
+from app.models import Variation, Project, Item, ProgressItem, Client, Superintendent
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -11,7 +11,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, Variation=Variation, Project=Project, Item=Item, ProgressItem=ProgressItem,
-                Client=Client)
+                Client=Client, Superintendent=Superintendent)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
